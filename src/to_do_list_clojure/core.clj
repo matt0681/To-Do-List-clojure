7ceb1d7d57b22301b6e9bb0216d64117e7bdc8b7
Matthew Lad <mattman678@gmail.com>
2021-04-30 22:53:58 -0500

    Replaced the gui nonsense with a simple console application.

--- src/to_do_list_clojure/core.clj
+++ src/to_do_list_clojure/core.clj
@@ -4,15 +4,39 @@
 ;;;;     4/29/2021                                            ;;;;
 ;;;;----------------------------------------------------------;;;;
 
-(ns to-do-list-clojure.core)
-(use '[to-do-list-clojure.data :as db])
-(use '[to-do-list-clojure.gui :as gui])
+(ns to-do-list-clojure.core
+  (:require [to-do-list-clojure.data :as db]
+            [clojure.string :as str]))
+
+
+
+(defn prompt-and-read
+  "Prompts the user for a command and returns what the user enters.
+   Returns nil if the command 'exit-to-do' is entered."
+  []
+  (print "[Please Enter a Command]> ")
+  (flush)
+  (let [input (read-line)]
+    (if (= input "exit-to-do")
+    nil
+    input)))
+
+
+(defn console-loop
+  ""
+  []
+  (loop [input (prompt-and-read)]
+    (-> input
+        str/upper-case
+        println)
+    (recur (prompt-and-read))))
 
 
 (defn -main
   [& Args]
 
-  ;; Creating the GUI
-  (gui/create-GUI)
+  ;; Create here a console application which creates a to-do list.
+  ;; Have a REPL Loop which displays the TO-do list and has commands to make changes.
 
   )
+
