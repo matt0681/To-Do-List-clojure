1e79ad78c9fba205524b503c0c85d0f047401d76
Matthew Lad <mattman678@gmail.com>
2021-04-30 21:25:03 -0500

    Started using cljfx instead of seesaw.

--- src/to_do_list_clojure/gui.clj
+++ src/to_do_list_clojure/gui.clj
@@ -5,27 +5,32 @@
 ;;;;----------------------------------------------------------;;;;
 
 (ns to-do-list-clojure.gui
-  (:use seesaw.core))
+  (:require [cljfx.api :as fx])
+  (:import [javafx.application Platform]
+           [javafx.scene.input KeyCode KeyEvent]
+           [javafx.scene.paint Color]
+           [javafx.scene.canvas Canvas])
+  (:gen-class))
 
-(defn create-GUI
-  ""
-  []
-  (native!)
+(def to-do-list-label
+  {:fx/type :label
+   :text "To-Do List"})
 
-  (def main-label
-    (label :text "Hi there!"
-           :border 5
-           :background "#888"
-           :foreground :blue))
+(def date-label
+  {:fx/type :label
+   :text "Date"})
 
-  (def main-frame
-    (frame :title "Lad's To-Do List!"
-           :on-close :exit
-           :content main-label
-           :width 640
-           :height 480
-           :resizable? true))
-  
-  (-> main-frame show!))
+(def entry-label
+  {:fx/type :label
+   :text "Entry"})
 
+(def renderer
+  (fx/create-renderer))
 
+(renderer
+  {:fx/type :stage
+   :showing true
+   :scene {:fx/type :scene
+           :root {:fx/type :v-box
+                  :children [date-label
+                             entry-label]}}})
